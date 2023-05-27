b752eb89019f4c0a7a9dab42a3160aa5c4b33b12
AlyonaCrainova <alyonacrainova@icloud.com>
2023-05-27 16:09:50 +0300

    add two-digit number support
    add two-digit number support and fix input validation

--- src/main/java/task_4/PostfixStatement.java
+++ src/main/java/task_4/PostfixStatement.java
@@ -1,35 +1,36 @@
 package task_4;
 
+import java.util.*;
 import java.util.Stack;
 
 public class PostfixStatement {
     private Double previousResult;
 
-    public Double calculate(String input) throws NumberFormatException {
-        String reversedInput = new StringBuilder(input).reverse().toString();
+    public Double calculate(String input) throws InvalidInputException, NumberFormatException {
+        if (!input.matches("[0-9]+(\\s[0-9]+\\s[\\-+/*=](\\s=)?)+")) {
+            throw new InvalidInputException("Form of " + input + "is prohibited");
+        }
         Stack<String> stack_1 = new Stack<>();
         Stack<Double> stack_2 = new Stack<>();
-        for (String operand : reversedInput.split(" ")) {
+        List<String> reversedInput = Arrays.asList(input.split(" "));
+        Collections.reverse(reversedInput);
+        for (String operand : reversedInput) {
             stack_1.push(operand);
         }
         while (!stack_1.isEmpty()) {
             String operand = stack_1.pop();
             switch (operand) {
-                case "+" -> {
-                    stack_2.push(stack_2.pop() + stack_2.pop());
-                }
+                case "+" -> stack_2.push(stack_2.pop() + stack_2.pop());
                 case "-" -> {
                     Double subtrahend = stack_2.pop();
                     Double minuend = stack_2.pop();
                     stack_2.push(minuend - subtrahend);
                 }
-                case "*" -> {
-                    stack_2.push(stack_2.pop() * stack_2.pop());
-                }
+                case "*" -> stack_2.push(stack_2.pop() * stack_2.pop());
                 case "/" -> {
                     Double divisor = stack_2.pop();
                     Double dividend = stack_2.pop();
-                    stack_2.push(dividend/divisor);
+                    stack_2.push(dividend / divisor);
                 }
                 case "=" -> {
                     return stack_2.pop();
@@ -41,7 +42,7 @@ public class PostfixStatement {
         return null;
     }
 
-    public Double getPreviousResult(){
+    public Double getPreviousResult() {
         return previousResult;
     }
 }
--- src/test/java/task_4/PostfixStatementTest.java
+++ src/test/java/task_4/PostfixStatementTest.java
@@ -8,51 +8,57 @@ public class PostfixStatementTest {
     private final PostfixStatement statement = new PostfixStatement();
 
     @Test
-    void calculate_without_return_value() {
+    void calculate_without_return_value() throws InvalidInputException {
         Double result = statement.calculate("1 2 + 3 *");
         assertNull(result);
         assertEquals(9, statement.getPreviousResult());
     }
 
     @Test
-    void calculate_with_return_value() {
+    void calculate_with_return_value() throws InvalidInputException {
         Double result = statement.calculate("8 2 + 5 * 9 + =");
         assertEquals(59, result);
     }
 
     @Test
     void calculate_invalid_input_unsupported_form() {
-        Exception exception = assertThrows(NumberFormatException.class, () -> {
-            statement.calculate("3 10 15 − *");
+        Exception exception = assertThrows(InvalidInputException.class, () -> {
+            statement.calculate("3 10 15 - *");
         });
         assertNotNull(exception);
     }
 
     @Test
     void calculate_invalid_input_doubles() {
-        Exception exception = assertThrows(NumberFormatException.class, () -> {
-            statement.calculate("3.20 10 15 − *");
+        Exception exception = assertThrows(InvalidInputException.class, () -> {
+            statement.calculate("3.20 10 − 15 *");
         });
         assertNotNull(exception);
     }
 
     @Test
     void calculate_invalid_input_empty() {
-        Exception exception = assertThrows(NumberFormatException.class, () -> {
+        Exception exception = assertThrows(InvalidInputException.class, () -> {
             statement.calculate("");
         });
         assertNotNull(exception);
     }
 
     @Test
-    void calculate_division() {
+    void calculate_division() throws InvalidInputException {
         Double result = statement.calculate("8 2 + 5 * 5 / =");
         assertEquals(10, result);
     }
 
     @Test
-    void calculate_subtraction() {
+    void calculate_subtraction() throws InvalidInputException {
         Double result = statement.calculate("8 2 + 5 * 5 - =");
         assertEquals(45, result);
+    }
+
+    @Test
+    void calculate_two_digit_number() throws InvalidInputException {
+        Double result = statement.calculate("18 2 + 5 * 5 - =");
+        assertEquals(95, result);
     }
 }
