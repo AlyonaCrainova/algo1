16eb2c8a6c4e9350586df0431fc5aa9c031056d2
AlyonaCrainova <alyonacrainova@icloud.com>
2023-05-30 13:44:08 +0300

    remove redundant check

--- src/main/java/task_5/QueueRotator.java
+++ src/main/java/task_5/QueueRotator.java
@@ -4,9 +4,6 @@ import java.util.Queue;
 
 public class QueueRotator {
     public void rotate(Queue<Integer> queue, int number){
-        if (queue.isEmpty()){
-            return;
-        }
         for(int i = 0; i < number; i++){
             queue.add(queue.poll());
         }
