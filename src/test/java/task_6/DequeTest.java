4d323eb85e61b0090f8d75b4f509cb60bf5ba748
AlyonaCrainova <alyonacrainova@icloud.com>
2023-05-30 13:50:14 +0300

    rename

--- src/test/java/task_6/DequeTest.java
+++ src/test/java/task_6/DequeTest.java
@@ -5,73 +5,73 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DequeTest {
-    private final Deque<Integer> queue = new Deque<>();
+    private final Deque<Integer> deque = new Deque<>();
 
     @Test
-    void addFront_to_empty_queue() {
-        queue.addFront(1);
+    void addFront_to_empty() {
+        deque.addFront(1);
 
-        assertEquals(1, queue.size());
-        assertTrue(queue.contains(1));
+        assertEquals(1, deque.size());
+        assertTrue(deque.contains(1));
     }
 
     @Test
-    void addFront_to_non_empty_queue() {
-        queue.addFront(1);
-        queue.addFront(2);
+    void addFront_to_non_empty() {
+        deque.addFront(1);
+        deque.addFront(2);
 
-        assertEquals(2, queue.size());
-        assertTrue(queue.contains(2));
+        assertEquals(2, deque.size());
+        assertTrue(deque.contains(2));
     }
 
     @Test
-    void removeFront_from_empty_queue() {
-        assertNull(queue.removeFront());
-        assertEquals(0, queue.size());
+    void removeFront_from_empty() {
+        assertNull(deque.removeFront());
+        assertEquals(0, deque.size());
     }
 
     @Test
-    void removeFront_from_non_empty_queue() {
-        queue.addFront(1);
-        queue.addFront(2);
+    void removeFront_from_non_empty() {
+        deque.addFront(1);
+        deque.addFront(2);
 
-        assertEquals(2, queue.size());
-        assertEquals(2, queue.removeFront());
-        assertEquals(1, queue.size());
-        assertFalse(queue.contains(2));
+        assertEquals(2, deque.size());
+        assertEquals(2, deque.removeFront());
+        assertEquals(1, deque.size());
+        assertFalse(deque.contains(2));
     }
 
     @Test
-    void addTail_to_empty_queue() {
-        queue.addTail(1);
+    void addTail_to_empty() {
+        deque.addTail(1);
 
-        assertEquals(1, queue.size());
-        assertTrue(queue.contains(1));
+        assertEquals(1, deque.size());
+        assertTrue(deque.contains(1));
     }
 
     @Test
-    void addTail_to_non_empty_queue() {
-        queue.addTail(1);
-        queue.addTail(2);
+    void addTail_to_non_empty() {
+        deque.addTail(1);
+        deque.addTail(2);
 
-        assertEquals(2, queue.size());
-        assertEquals(2, queue.removeTail());
+        assertEquals(2, deque.size());
+        assertEquals(2, deque.removeTail());
     }
 
     @Test
-    void removeTail_from_empty_queue() {
-        assertNull(queue.removeTail());
+    void removeTail_from_empty() {
+        assertNull(deque.removeTail());
     }
 
     @Test
-    void removeTail_from_non_empty_queue() {
-        queue.addTail(1);
-        queue.addTail(2);
+    void removeTail_from_non_empty() {
+        deque.addTail(1);
+        deque.addTail(2);
 
-        assertEquals(2, queue.size());
-        assertEquals(2, queue.removeTail());
-        assertEquals(1, queue.size());
-        assertFalse(queue.contains(2));
+        assertEquals(2, deque.size());
+        assertEquals(2, deque.removeTail());
+        assertEquals(1, deque.size());
+        assertFalse(deque.contains(2));
     }
 
 }
